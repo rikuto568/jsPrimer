c0018786e6a70cb1eff72fef1114131ba7d89f69
rikuto <rikuto042700@icloud.com>
2026-02-21 19:10:54 +0900

    cliの機能は完成かな

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,14 +1,24 @@
 import * as util from "node:util";
 import * as fs from "node:fs/promises";
+import { marked } from "marked";
 
-const { positionals } = util.parseArgs({
+const { values, positionals } = util.parseArgs({
   allowPositionals: true,
+  options: {
+    gfm: {
+      type: "boolean",
+      default: false,
+    },
+  },
 });
 const filePath = positionals[0];
 // ファイルをUTF-8として非同期で読み込む
 fs.readFile(filePath, { encoding: "utf8" })
   .then((file) => {
-    console.log(file);
+    const html = marked.parse(file, {
+      gfm: values.jfm,
+    });
+    console.log(html);
   })
   .catch((err) => {
     console.error(err.message);
